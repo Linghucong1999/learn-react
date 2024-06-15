7d5145f05bb86247db586a6db9f452845cf4f67b
mika <2865911620@qq.com>
2024-06-16 00:59:49 +0800

    feat: ui树具体化

--- src/react_ui_tree/InspirationGenerator.js
+++ src/react_ui_tree/InspirationGenerator.js
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import quotes from './quotes';
 import FancyText from './FancyText';
+import inspirations from './inspirations';
+import Color from './Color';
 
 export default function InspirationGenerator({ children }) {
     const [index, setIndex] = React.useState(0);
     const quote = quotes[index];
+    const inspiration = inspirations[index];
     const next = () => setIndex((index + 1) % quotes.length);
 
     return (
         <>
-            <p>你的下一个选择是</p>
+            <p>你的下一个选择是 {inspiration.type}</p>
+            {inspiration.type === 'quote' ? <FancyText text={inspiration.value}></FancyText> : <Color value={inspiration.value}></Color>}
             <FancyText text={quote}></FancyText>
             <button onClick={next}>下一个</button>
             {children}
